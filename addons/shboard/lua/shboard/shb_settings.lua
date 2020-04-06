3adac572e18f7208bc69f35a5fe45f21ad5b109d
Daniel <63028728+akersda@users.noreply.github.com>
2020-04-06 13:09:20 +0930

    Update shb_settings.lua

--- addons/shboard/lua/shboard/shb_settings.lua
+++ addons/shboard/lua/shboard/shb_settings.lua
@@ -1,3 +1,5 @@
+local SHB = GetSHBTable()
+
 SHB:ResetSettings() -- refeshes tables so that settengs can be changed on live server
 
 -- board screen space
@@ -6,16 +8,15 @@ SHB.setting.length = 0.7 * ScrH()
 
 -- title lines
 SHB.setting.title = "Vulcan Network"
-SHB:AddLine( "{OSTIME}" , "" , "Map: " .. SHB:GetFormattedMap() )
+SHB:AddLine( function() return os.date( "%H:%M:%S" , os.time()) end , "" , "Map: " .. SHB:GetFormattedMap() )
 
 -- teams
-SHB.setting.layout = "teams" -- either 'teams' or 'list'
-
 SHB:AddTeam( 1, false, "Spectate", Color(240,240,240) )
 SHB:AddTeam( 2, true )
 SHB:AddTeam( 3, true )
 
 -- player
-SHB:AddText( "P", function(ply) return ply:Ping() end, 20 ) -- will change to hook
-SHB:AddText( "K", function(ply) return ply:Frags() end, 20 )
-SHB:AddText( "D", function(ply) return ply:Deaths() end, 20 )
+SHB:AddRText( "P", function(ply) return ply:Ping() end, 20 )
+SHB:AddRText( "K", function(ply) return ply:Frags() end, 20 )
+SHB:AddRText( "D", function(ply) return ply:Deaths() end, 20 )
+SHB:AddRText( "pos", function(ply) return math.Round(ply:GetPos().x) end, 40 )
